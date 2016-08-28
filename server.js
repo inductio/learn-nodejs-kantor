469104464fdeb45ea0a4eefb539f00b62628f868
alex <alex.shustrov@gmail.com>
2016-08-29 02:52:49 +0300

    lesson 14 - 15 // http server

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-var db = require('db');
-db.connect();
+var http = require('http');
+var url = require('url');
 
-var User = require('./user');
+//127.0.0.1:1337/echo?message=Hello
 
-function run() {
-    var vasya = new User ('Вася');
-    var petya = new User ('Петя');
-    vasya.hello(petya);
 
-    console.log(db.getPhrase("Run successful"));
-}
+var server = new http.Server(function (req, res) {
 
-if (module.parent) {
-    exports.run = run;
-} else {
-    run();
-}
+    res.writeHead(200, "OK", {
+        "Cache-control": "no-cache",
+        "Content-Type" : "application/json; charset=utf-8"
+    });
+
+  //  res.setHeader('Cache-control', 'no-cache');
+  //  res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.end( " Ответ: " + urlParsed.query.message )
+    } else {
+        res.statusCode = 404;
+        res.end( "Page not found / Страница не найдена" );
+    }
+});
+
+server.listen(1337, '127.0.0.1');
