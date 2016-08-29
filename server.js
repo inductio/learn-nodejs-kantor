5853f22bcd29f3c7f6fe3e44e0b79a32fa3fc3c7
Aleksandr Shustrov <aleksandr.shustrov@gmail.com>
2016-08-29 15:32:29 +0300

    refactoring

--- server.js
+++ server.js
@@ -2,72 +2,37 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function errorHandler(err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end('На сервере ошибкэ!');
+    return;
+}
+
 http.createServer(function(req, res){
+
     var info;
 
     if (req.url === '/'){
-
         fs.readFile('index.html', function (err, info) {
-            if(err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end('На сервере ошибкэ!');
-                return;
-            }
-
-            res.end(info);
+            err ? errorHandler(err) : res.end(info);
         });
     } else if (req.url === '/about'){
-
         fs.readFile('about.html', function (err, info) {
-            if(err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end('На сервере ошибкэ!');
-                return;
-            }
-
-            res.end(info);
+            err ? errorHandler(err) : res.end(info);
         });
     } else if (req.url === '/blog'){
-
         fs.readFile('blog.html', function (err, info) {
-            if(err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end('На сервере ошибкэ!');
-                return;
-            }
-
-            res.end(info);
+            err ? errorHandler(err) : res.end(info);
         });
     } else if (req.url === '/contacts'){
-
         fs.readFile('contacts.html', function (err, info) {
-            if(err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end('На сервере ошибкэ!');
-                return;
-            }
-
-            res.end(info);
+            err ? errorHandler(err) : res.end(info);
         });
     } else {
-
         fs.readFile('404.html', function (err, info) {
-            if(err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.end('На сервере ошибкэ!');
-                return;
-            }
-
-            res.end(info);
+            err ? errorHandler(err) : res.end(info);
         });
-
-
         res.statusCode = 404;
-
     }
 }).listen(3000, '127.0.0.1');
